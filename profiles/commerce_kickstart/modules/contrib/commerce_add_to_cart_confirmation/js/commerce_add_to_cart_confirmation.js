0d13aa3967562f49efa153a6532abdb8385e67c4
Matt Glaman <nmd.matt@gmail.com>
2018-02-27 17:02:09 -0600

    Update Commerce Kickstart to 7.x-2.52

--- profiles/commerce_kickstart/modules/contrib/commerce_add_to_cart_confirmation/js/commerce_add_to_cart_confirmation.js
+++ profiles/commerce_kickstart/modules/contrib/commerce_add_to_cart_confirmation/js/commerce_add_to_cart_confirmation.js
@@ -1,15 +1,29 @@
 (function ($) {
   Drupal.behaviors.commerce_add_to_cart_confirmation_overlay = {
     attach:function (context, settings) {
+      var overlayClass = 'commerce_add_to_cart_confirmation_overlay';
+      var overlayParentSelector = 'body';
+
+      // Determine the appropriate overlay class and parent selector based on the settings array.
+      if (typeof settings.commerceAddToCartConfirmation != 'undefined') {
+        if (typeof settings.commerceAddToCartConfirmation.overlayClass != 'undefined') {
+          overlayClass = settings.commerceAddToCartConfirmation.overlayClass;
+        }
+
+        if (typeof settings.commerceAddToCartConfirmation.overlayParentSelector != 'undefined') {
+          overlayParentSelector = settings.commerceAddToCartConfirmation.overlayParentSelector;
+        }
+      }
+
       if ($('.commerce-add-to-cart-confirmation').length > 0) {
         // Add the background overlay.
-        $('body').append("<div class=\"commerce_add_to_cart_confirmation_overlay\"></div>");
+        $(overlayParentSelector).append('<div class="' + overlayClass + '"></div>');
 
         // Enable the close link.
-        $('.commerce-add-to-cart-confirmation-close').live('click touchend', function(e) {
+        $('.commerce-add-to-cart-confirmation-close').bind('click touchend', function(e) {
           e.preventDefault();
           $('.commerce-add-to-cart-confirmation').remove();
-          $('.commerce_add_to_cart_confirmation_overlay').remove();
+          $('.' + overlayClass).remove();
         });
       }
     }
